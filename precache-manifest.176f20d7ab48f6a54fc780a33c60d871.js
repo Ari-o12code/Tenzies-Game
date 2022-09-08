4e32f9d3cb5597090b9663d84e3d0a269a543c0c
Quadri Alameen <quadrameen@gmail.com>
2022-09-08 15:52:01 +0100

    Updates

---
+++ precache-manifest.176f20d7ab48f6a54fc780a33c60d871.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "72dc339b668a08833fb8",
+    "url": "/Tenzies-Game/static/css/main.68a92a88.chunk.css"
+  },
+  {
+    "revision": "72dc339b668a08833fb8",
+    "url": "/Tenzies-Game/static/js/main.72dc339b.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/Tenzies-Game/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "a1d00c6dd1af6c5370b7",
+    "url": "/Tenzies-Game/static/css/2.d8a68671.chunk.css"
+  },
+  {
+    "revision": "a1d00c6dd1af6c5370b7",
+    "url": "/Tenzies-Game/static/js/2.a1d00c6d.chunk.js"
+  },
+  {
+    "revision": "f49e5bc8d04e4a229a5d",
+    "url": "/Tenzies-Game/static/js/runtime~main.f49e5bc8.js"
+  },
+  {
+    "revision": "9938963863cd03e2d32bf22c049de963",
+    "url": "/Tenzies-Game/index.html"
+  }
+];
